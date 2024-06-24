84d27316f79c16c62d42e2b291ce6dd8b57d909d
Akshay Mahalle <akshay.mahalle8329@gmail.com>
2024-06-24 11:08:51 -0700

    chnages in the path files

--- launch/box_launch.py
+++ launch/box_launch.py
@@ -2,11 +2,14 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-# from ament_index_python.packages import get_package_share_directory
-import os 
-def generate_launch_description():
+import os
 
-    config_directory = '/home/akshay/ros2_ws/virtual_shake_robot_pybullet/config'
+def generate_launch_description():
+    
+    ros2_ws = os.getenv('ROS2_WS', default=os.path.expanduser('~/ros2_ws'))
+
+    
+    config_directory = os.path.join(ros2_ws, 'virtual_shake_robot_pybullet/config')
 
     physics_engine_parameters_path = os.path.join(config_directory, 'physics_engine_parameters.yaml')
     physics_parameters_path = os.path.join(config_directory, 'physics_parameters.yaml')
@@ -18,20 +21,22 @@ def generate_launch_description():
         package='virtual_shake_robot_pybullet',  
         executable='simulation_node.py',  
         name='simulation_node', 
-        output='screen' ,
-        parameters= [physics_engine_parameters_path,physics_parameters_path,vsr_structure_path,pbr_structure_path,pbr_mesh_structure_path]
+        output='screen',
+        parameters=[
+            physics_engine_parameters_path,
+            physics_parameters_path,
+            vsr_structure_path,
+            pbr_structure_path,
+            pbr_mesh_structure_path
+        ]
     )
-
 
     control_node = Node(
-
         package='virtual_shake_robot_pybullet',  
         executable='control_node.py',  
-        name= 'control_node' ,
-        output = 'screen' ,
-    
+        name='control_node',
+        output='screen'
     )
-    
 
     return LaunchDescription([
         simulation_node,
